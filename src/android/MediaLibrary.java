8106d760b8a5e29b7e8d9913e77537932e9d2425
Philipp Grosswiler <philipp.grosswiler@gmail.com>
2014-06-23 11:28:13 +0700

    Added missing import.

--- src/android/MediaLibrary.java
+++ src/android/MediaLibrary.java
@@ -26,6 +26,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.Uri;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
